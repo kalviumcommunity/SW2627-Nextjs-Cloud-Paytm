802d94419b150092f85e79c86b1e52c9758d7bc9
B-Payal <b08.payal@gmail.com>
2026-07-16 14:51:13 +0530

    feat:proxy implemented and recharge history component added

---
+++ paytm/src/proxy.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export function proxy(req) {
+    console.log("Proxy is running");
+
+    const token = req.cookies.get("token")?.value;
+
+    console.log("Token:", token);
+
+    if (!token) {
+        console.log("No token");
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log("Verified:", decoded);
+
+        return NextResponse.next();
+    } catch (err) {
+        console.log("JWT Error:", err.message);
+
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"],
+};
