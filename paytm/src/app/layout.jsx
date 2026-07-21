253e770529c2750eebf49bfa1c893646b2dcf3ae
aayaanchoudhary@gmail.com <aayaanchoudhary29@gmail.com>
2026-07-21 15:41:24 +0530

    Feat:Added Toasts

--- paytm/src/app/layout.jsx
+++ paytm/src/app/layout.jsx
@@ -1,9 +1,37 @@
 import "./globals.css"
-
+import { Toaster } from "react-hot-toast";
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        {children}
+        <Toaster
+  position="top-center"
+  toastOptions={{
+    duration: 3000,
+    style: {
+      minWidth: "400px",
+      padding: "18px 24px",
+      fontSize: "17px",
+      borderRadius: "10px",
+      background: "#1f2937",
+      color: "#fff",
+    },
+    success: {
+      iconTheme: {
+        primary: "#22c55e",
+        secondary: "#fff",
+      },
+    },
+    error: {
+      iconTheme: {
+        primary: "#ef4444",
+        secondary: "#fff",
+      },
+    },
+  }}
+/>
+      </body>
     </html>
   );
 }
